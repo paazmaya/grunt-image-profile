bff52ae6b4f866675bce1c9c92ca15f82685cfcb
Juga Paazmaya <paazmaya@yahoo.com>
2016-07-15 13:59:23 +0300

    Secure every :pig:

--- tasks/image_profile.js
+++ tasks/image_profile.js
@@ -2,7 +2,7 @@
  * grunt-image-profile
  * https://github.com/paazmaya/grunt-image-profile
  *
- * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (http://paazmaya.fi)
+ * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (https://paazmaya.fi)
  * Licensed under the MIT license.
  */
 'use strict';
